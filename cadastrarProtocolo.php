eb4f9bb60cb4abd5cf8d92ec2d0e146b44869f39
Tiago <52145562+tiagoutzfoscarini@users.noreply.github.com>
2022-11-29 23:18:52 -0300

    Remoção validação CPF

--- cadastrarProtocolo.php
+++ cadastrarProtocolo.php
@@ -1,6 +1,5 @@
 <?php
 $utils = include ('php/php_utils.php');
-$config = include('php/config.php');
 ?>
 <!DOCTYPE html>
 <html lang="br">
@@ -41,11 +40,11 @@ $config = include('php/config.php');
         <br/>
         <label class="description">Descrição do problema*:<input id="description" name="description" type="text" placeholder="Conte o que aconteceu..." required ></label>
         <br/>
-        <label class="name">Nome completo*:<input id="name" name="name" type="text" pattern="[A-Za-zÀ-ÖØ-öø-ÿ ]+" placeholder="Informe seu nome completo..." required ></label>
+        <label class="name">Nome completo*:<input id="name" name="name" type="text" placeholder="Informe seu nome completo..." required ></label>
         <br/>
         <label class="cpf">CPF*:<input id="cpf" name="cpf" type="text" placeholder="Informe seu CPF: 00000000000 (sem pontos, espaços ou traços)" required ></label>
         <br/>
-        <label class="phone">Telefone para contato*:<input id="tel" name="tel" type="tel" size="11" pattern="[0-9]{2}[0-9]{9}" placeholder="Informe seu telefone no formato 519xxxxxxxx (sem pontos, espaços ou traços)" required ></label>
+        <label class="phone">Telefone para contato*:<input id="tel" name="tel" type="tel" size="11" placeholder="Informe seu telefone no formato 519xxxxxxxx (sem pontos, espaços ou traços)" required ></label>
         <br/>
         <label class="email">Email para contato:<input id="email" name="email" type="email" placeholder="xxxxx@xxxxx.xxx" ></label>
         <br/>
@@ -69,13 +68,13 @@ $config = include('php/config.php');
 <!--        <label class="map">Mapa: <input id="map" type="" disabled /></label>-->
 <!--        <br/>-->
         <p class="notes">* campos obrigatórios</p>
+        <br/>
         <input class='button' type="submit" name="submit" value="Enviar solicitação"/>
     </form>
 </div>
 <?php
 // Submit do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $errors = [];
 
     $form_problemType = $_POST['problemType'];
     $form_description = $_POST['description'];
@@ -97,15 +96,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $form_email = isset($_POST['email']) ? $_POST['email'] : false;
     $form_images = isset($_POST['images']) ? $_POST['images'] : false;
 
-    $form_cpf = validate_CPF(str_replace(" ", "", $form_cpf));
+//    $form_cpf = validate_CPF(str_replace(" ", "", $form_cpf));
 //    $form_tel = validate_phone(str_replace(" ", "", $form_tel));
 
     $conn = sql_connect();
 
-    if ($form_problemType && $form_description && $form_name && $form_cpf && $form_tel && $form_city && $form_address) {
+    if (!empty($form_problemType) && !empty($form_description) && !empty($form_name) && !empty($form_cpf) && !empty($form_tel) && !empty($form_city) && !empty($form_address)) {
         $sql = "INSERT INTO protocols (protocolProblemTypeId, protocolDescription, protocolRequesterName, protocolRequesterCpf, protocolRequesterPhone, protocolRequesterEmail, protocolCityId, protocolAddress, protocolStatusId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-//        $sql = "INSERT INTO protocols ('description', 'problemTypeId', 'cityId', 'address', 'statusId', 'requesterCpf', 'requesterName', 'requesterPhone') VALUES ('$form_description', '$form_description', '$form_city', '$form_address', 1, '$form_cpf', '$form_name', '$form_tel')";
 
         $params = array($form_problemType, $form_description, $form_name, $form_cpf, $form_tel, $form_email, $form_city, $form_address, 1);
 
@@ -114,12 +111,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($stmt) {
             echo "<script>alert('Protocolo cadastrado com sucesso!');</script>";
         } else {
-//            echo "<script type='text/javascript'> alert('".json_encode($errors)."') </script>";
             echo "<script>alert('Erro ao cadastrar protocolo! Falha ao inserir no banco!');</script>";
         }
     } else {
-//        echo "<script type='text/javascript'> alert('".json_encode($errors)."') </script>";
-        echo "<script>alert('Erro ao cadastrar protocolo! Valores faltando!');</script>";
+        echo "<script>alert('Erro ao cadastrar protocolo! Valores faltando ou incorretos!');</script>";
     }
 
     sqlsrv_close($conn);
--- consultarProtocolo.php
+++ consultarProtocolo.php
@@ -51,7 +51,7 @@ $config = include('php/config.php');
             echo "<label class='labelProtocol'>Descrição: <p class='labelProtocolValue'>{$row['protocolDescription']}</p></label><br/>";
             echo "<label class='labelProtocol'>Aberto por: <p class='labelProtocolValue'>{$row['protocolRequesterName']}</p></label><br/>";
             echo "<label class='labelProtocol'>Status: <p class='labelProtocolValue'>{$row['statusName']}</p></label>";
-            echo "<td class='cityBarItem'><button id='buttonChangeStatus' class='button' ><a href='consultarProtocolo.php?changeStatus=true&protocolId={$row['protocolId']}'>Alterar status</a></button></td>";
+            echo "<td class='cityBarItem'><button id='buttonChangeStatus' class='button' ><a href='consultarProtocolo.php?id={$id}&changeStatus=true&protocolId={$row['protocolId']}'>Alterar status</a></button></td>";
 //            echo "<td class='cityBarItem'><button id='buttonChangeStatus' class='button' onclick=";
 //            echo sql_update_protocol_Status($conn, $row['protocolId']);
 //            echo " >Alterar status</button></td>";
